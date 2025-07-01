bb44474e408187353fd33df429797ecbef05ba9c
MagicalMegaDev <klay88888@gmail.com>
2025-07-01 13:41:25 -0500

    BaseCharacterInit Fixed

--- Assets/Scripts/Characters/BaseCharacter.gd
+++ Assets/Scripts/Characters/BaseCharacter.gd
@@ -1,28 +1,30 @@
 class_name BaseCharacter extends CharacterBody2D 
 
 #Collection of Potential Handlers Goes here
-var movementHandler:MovementHandler
+var movement_handler:MovementHandler
 
 func _enter_tree():
 	#Gather up Handlers and assign them
-	movementHandler = ApplyHandler("MovementHandler")
+	movement_handler = ApplyHandler(MovementHandler)
 
-func ApplyHandler(className:String) -> Object:
-	var found:Object = null
+
+func ApplyHandler(handler_type) -> Object:
+	var found: Object = null
 	for child in GameManager.get_all_children(self):
-		print(child.name + child.get_class())
-		if child.is_class(className):
-			if(found):
-				print("Multiple" + className +" found at: " + self.name)
+		if is_instance_of(child, handler_type):
+			if found:
+				print("Multiple " + str(handler_type) + " found at: " + self.name)
 			else:
 				found = child
+	
 	return found
 
 func _ready():
-	if(movementHandler):
-		print(movementHandler.name)
+	if(movement_handler):
+		print(movement_handler.name)
 	else:
 		print("Nope")
+
 #_on_velocity_calculated(momentum)
 #momentum:Vector2 = The players current speed in x and y, to be assigned to velocity
 #This is simply a generic signal reciever to make the character move, agnostic of whether it is through input, AI, or otherwise.
