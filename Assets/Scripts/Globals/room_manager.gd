00489e396b7177e00dae8917042d54d67d3fca39
MagicalMegaDev <klay88888@gmail.com>
2025-07-18 04:48:00 -0500

    Refactored room_manager
    kinda lol

--- Assets/Scripts/Globals/room_manager.gd
+++ Assets/Scripts/Globals/room_manager.gd
@@ -1,5 +1,8 @@
+#RoomManager.gd
+# Global node responsible for broadcasting events related to room state.
 extends Node
 
+#Emitted when the current room has been cleared of enemies.
 signal room_cleared
 
 func _ready():
