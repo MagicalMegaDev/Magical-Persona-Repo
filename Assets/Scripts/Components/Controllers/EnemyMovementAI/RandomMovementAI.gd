482bc850d6e734b61a66d56801b47e78447d54fa
MagicalMegaDev <klay88888@gmail.com>
2025-07-20 05:19:17 -0500

    RandomMovementAI refactored

--- Assets/Scripts/Components/Controllers/EnemyMovementAI/RandomMovementAI.gd
+++ Assets/Scripts/Components/Controllers/EnemyMovementAI/RandomMovementAI.gd
@@ -1,12 +1,19 @@
-class_name RandomMovementAI extends EnemyMovementAI
+#RandomMovementAI.gd
+#Basic Enemy Pathing that randomly changes direction after a delay.
+class_name RandomMovementAI 
+extends EnemyMovementAI
 
+# Current movement direction for the enemy.
 var direction := Vector2.ZERO
+
+# Timer controlling how often the enemy picks a new direction
 var direction_change_timer:Timer
-#Values are Placeholders
+
+# Minimum and maximum seconds between direction changes.
 @export var direction_change_frequency_min:float = 0.5
 @export var direction_change_frequency_max:float = 1.5
 
-#Initialize the timer
+#Initialize the timer used for andom movement.
 func _ready():
 	direction_change_timer = Timer.new()
 	direction_change_timer.wait_time = randf_range(direction_change_frequency_min, direction_change_frequency_max)
@@ -14,38 +21,44 @@ func _ready():
 	direction_change_timer.timeout.connect(set_direction)
 	add_child(direction_change_timer)
 
-#Sets State to a reset state while it is not active
+# Reset state while this AI is inactive.
 func pause():
 	direction = Vector2.ZERO
 	direction_change_timer.stop()
 	direction_change_timer.wait_time = randf_range(direction_change_frequency_min, direction_change_frequency_max)
 
-#Resumes movement when the state is reactivated
+# Resumes movement when the state is reactivated
 func resume():
 	set_direction()
 	direction_change_timer.start()
 	
 
-#Generate a random movement direction for the enemy
+# Generate a random movement direction for the enemy
 func set_direction():
 	direction = Vector2.from_angle(randf() * 2.0 * PI)
 	direction_change_timer.wait_time = randf_range(direction_change_frequency_min, direction_change_frequency_max)
 
-#Returns the current movement direction, recalculating if stuck
+# Returns the current movement direction, recalculating if stuck
 func get_direction() -> Vector2:
+	if(!my_character):
+		push_warning("RandomMovementAI: my_character is null")
+		return Vector2.ZERO
+
 	if(my_character.is_on_ceiling() or my_character.is_on_wall() or my_character.is_on_floor()):
 		set_direction()
 		direction_change_timer.stop()
+		
 	if(direction_change_timer.is_stopped()):
 		direction_change_timer.start()
 	return direction
 
 	
-#reverse_direction()
-#Move in the opposite of the current direction
+
+# Reverses the current direction
 func reverse_direction():
 	direction = -direction
 	direction_change_timer.start()
 
+# Called when another enemy enters the detection area.
 func _on_enemy_nearby(area):
 	reverse_direction()
