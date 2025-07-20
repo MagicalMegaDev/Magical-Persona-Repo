dc1704130ee86864ea3824824b552a3f693e097c
MagicalMegaDev <klay88888@gmail.com>
2025-07-20 04:41:44 -0500

    Refactored EnemyAnimationController to be more accurate

--- Assets/Scripts/Components/Controllers/AnimationController.gd
+++
@@ -1,4 +0,0 @@
-class_name AnimationController extends AnimationPlayer
-
-func _on_take_damage():
-	play("hurt")
---
+++ Assets/Scripts/Components/Controllers/EnemyAnimationController.gd
@@ -0,0 +1,8 @@
+#DEBUG CLASS TO GIVE BASIC DAMAGE ANIMATION FOR PROTOTYPE FEEDBACK
+class_name EnemyAnimationController extends AnimationPlayer
+
+const DAMAGE_ANIM := "hurt" #Name of damage animation
+
+func _on_take_damage():
+	if(has_animation(DAMAGE_ANIM)):
+		play(DAMAGE_ANIM)
