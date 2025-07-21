d7b1a4f7fb08cb95a1aa86280149cfebbe701388
MagicalMegaDev <klay88888@gmail.com>
2025-07-21 03:06:16 -0500

    ContactDamageHandler Improvements

--- Assets/Scripts/Components/Handlers/EnemyAttacks/ContactDamageHandler.gd
+++ Assets/Scripts/Components/Handlers/EnemyAttacks/ContactDamageHandler.gd
@@ -1,9 +1,10 @@
+#Handles Damage delaing when an enemy HitBox overlaps a valid HurtBox
 class_name ContactDamageHandler 
 extends Node2D
 
-@export var attack_stats:AttackStats
-@export var hit_box:HitBox 
-@export var hit_groups:Array[String] = ["Players"]
+@export var attack_stats:AttackStats #The incoming attack's stats
+@export var hit_box: HitBox #HitBox of the attack
+@export var hit_groups:Array[String] = ["Players"] #Groups this handler can damage
 var _friendly_fire:bool = false
 @export var friendly_fire:bool = false: #Variable to enable and disable contact damage to other enemies
 	get: return _friendly_fire
@@ -15,22 +16,21 @@ var _friendly_fire:bool = false
 func _ready():
 	assert(attack_stats, "%s has no AttackStats attached to it's ContactDamageHandler!" % get_parent().name)
 	assert(hit_box, "%s has no HitBox attached to it's ContactDamageHandler!" % get_parent().name) #make sure the hurt box has been hooked up
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_check_hit(bodies, areas, position):
+	if not hit_box.collections_in_area.is_connected(_on_check_hit):
+		hit_box.collections_in_area.connect(_on_check_hit)
+		
+#Check all colliders inside the hitbox for any in hit_groups
+func _on_check_hit(bodies:Array, areas:Array, position:Vector2):
 	for area in areas:
 		if area is HurtBox:
 			for group in hit_groups:
 				if(area.is_in_group(group)):
 					area._on_hit(attack_stats, position)
-	
-#_on_friendly_fire_toggle
+					break
+
 #Simple function to add and remove Enemies from the hit group, saves retyping code in _ready()
 func _on_friendly_fire_toggle(value:bool):
-	if(friendly_fire == true and !hit_groups.has("Enemies")):
+	if(value and !hit_groups.has("Enemies")):
 		hit_groups.append("Enemies")
-	elif(friendly_fire == false and hit_groups.has("Enemies")):
+	elif(!value and hit_groups.has("Enemies")):
 		hit_groups.erase("Enemies")
