845ce1de2f207135d2d26f8bf442be2a3bbedacb
MagicalMegaDev <klay88888@gmail.com>
2025-07-25 06:08:13 -0500

    Starting to get pickups working

--- Assets/Scripts/Components/HitBoxComponents/Pickups/HealthPickup.gd
+++ Assets/Scripts/Components/HitBoxComponents/Pickups/HealthPickup.gd
@@ -1,4 +1,16 @@
 class_name HealthPickup
 extends Pickup
 
-var heal_value := 1 #Amount of health this pickup heals
+const FULL_HEART_FRAME := 0
+const HALF_HEART_FRAME := 15
+
+enum HeartValue {HALF_HEART = 1, FULL_HEART = 2}
+@export var heal_value: HeartValue = HeartValue.HALF_HEART #Amount of health this pickup heals
+
+func _ready():
+	#Set the frame based on the pickups value
+	match heal_value:
+		1:
+			my_sprite.frame = HALF_HEART_FRAME
+		2:
+			my_sprite.frame = FULL_HEART_FRAME
--- Assets/Scripts/Components/HitBoxComponents/Pickups/Pickup.gd
+++ Assets/Scripts/Components/HitBoxComponents/Pickups/Pickup.gd
@@ -1,15 +1,23 @@
 #Generic container for all pickups
 class_name Pickup
-extends HitBox
+extends Node
+
+@onready var hit_box:HitBox = get_parent()
+@onready var my_sprite :Sprite2D = get_parent().get_node("Sprite2D")
 
 signal try_pickup(me:Pickup)
 
 func _ready():
-	body_entered.connect(_on_body_entered)
+	hit_box.collision_layer = 0
+	hit_box.set_collision_layer_value(12, true)
+	hit_box.collision_mask = 0
+	hit_box.set_collision_mask_value(4, true)
+	hit_box.body_entered.connect(_on_body_entered)
 	try_pickup.connect(SignalBus._on_item_pickup)
 
 func _on_body_entered(body:Node):
 	if(body.is_in_group("Players")):
+		print("Player Entered!")
 		try_pickup.emit(self)
 		
 func _on_pickup_attemped(picked_up:bool):
