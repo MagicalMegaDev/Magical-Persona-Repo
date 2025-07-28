50479e4a3e3420a472f976430e5facb82d26e132
MagicalMegaDev <klay88888@gmail.com>
2025-07-28 09:07:16 -0500

    Not rotating correctly

--- Assets/Scenes/Muzzle.gd
+++ Assets/Scenes/Muzzle.gd
@@ -4,13 +4,16 @@ extends RayCast2D
 @export var default_behavior:BulletMovementBehavior
 var ray_length: float
 var direction: Vector2
+var base_direction:Vector2
 
 func _ready():
 	ray_length = target_position.length()
-	direction = target_position.normalized()
+	base_direction = target_position.normalized()
+	direction = base_direction
 	
 func set_direction(new_dir:Vector2) -> void:
-	direction = new_dir.normalized()
+	var ang = Vector2.UP.angle_to(new_dir.normalized())
+	direction = base_direction.rotated(ang)
 	target_position = direction * ray_length
 
 
